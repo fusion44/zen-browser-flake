b2b3bf931b7742b2c2ed5f6623124301a1fc37e7
fusion44 <git.bleep096@passmail.net>
2025-09-14 18:50:30 +0200

    feat: update to 1.15.5b

--- flake.nix
+++ flake.nix
@@ -10,11 +10,11 @@
     nixpkgs,
   }: let
     system = "x86_64-linux";
-    version = "1.14.11b";
+    version = "1.15.5b";
     downloadUrl = {
       "specific" = {
         url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-        sha256 = "sha256:06rx4hb6d1gy1kja73cmkywyvp5i0mb4szv1drih9xxla4ik8lsh";
+        sha256 = "sha256:0rychb5xhc1alr50mcf2lj6shy0shch8pqcxkvhylqgzdm6p4hwd";
       };
     };
 
