b4f73dad09782acb5629332d12e8e22fc144994f
fusion44 <git.bleep096@passmail.net>
2026-03-17 07:42:14 +0100

    feat: update to 1.19.3b

--- flake.nix
+++ flake.nix
@@ -10,11 +10,11 @@
     nixpkgs,
   }: let
     system = "x86_64-linux";
-    version = "1.19b";
+    version = "1.19.3b";
     downloadUrl = {
       "specific" = {
         url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-        sha256 = "sha256:1bdrgcrsjw7n5afbrgqpybf4p4agzd7xa00ggxbyzqasnskdqyxr";
+        sha256 = "sha256:0x7s0jwgai7vadb0mcwyjvp9pgy93r6n8av9dkxvgas4sk0awsyd";
       };
     };
 
